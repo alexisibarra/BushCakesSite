fd96d33b7b65e67574d945697ee9a887c0835384
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-07-11 15:58:30 -0400

    Refactorizando el home y cambiando el layout de las secciones

---
+++ site/php/home/hero.php
@@ -0,0 +1,30 @@
+<div class="container" style="margin-bottom: 50px">
+    <div class="row">
+        <div class="col-sm-8" style="padding-right: 5px">
+            <div class="owl-carousel owl-carousel-fullwidth animate-box">
+                <? for ($i = 1; $i <= 10; $i++){?>
+                    <div class="item">
+                        <a href="http://placehold.it/630x480" class="image-popup">
+                            <img src="http://placehold.it/630x480" alt="image">
+                        </a>
+                    </div>
+                <? } ?>
+            </div>
+        </div>
+        <div class="col-sm-4" style="padding-left: 0px; margin-bottom: 5px">
+            <div class="animate-box">
+                <a href="http://order.carlosbakery.com">
+                    <img src="http://placehold.it/300x238" alt="Order Online, Pick Up In-store" title="Order Online, Pick Up In-store">
+                </a>
+            </div>
+        </div>
+        <div class="col-sm-4" style="padding-left: 0px">
+            <div class="animate-box">
+                <a href="http://order.carlosbakery.com">
+                    <img src="http://placehold.it/300x238" alt="Order Online, Pick Up In-store" title="Order Online, Pick Up In-store">
+                </a>
+            </div>
+        </div>
+        <!-- End Slider -->
+    </div>
+</div>
