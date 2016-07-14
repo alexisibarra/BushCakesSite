b01e567a86de2fcc08dc7ce616a9afa82931c101
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-07-14 14:42:55 -0400

    Insertando los subelementos de los items del menu

--- views/layout/menu.php
+++ views/layout/menu.php
@@ -1,29 +1,66 @@
+<div class="fluid-container menu-container">
 
-<div id="fh5co-menuwrap" role="navigation" class="sticker text-center visible-sm visible-md visible-lg">
     <div class="container">
-        <a href="/" style="border: none;">
-            <img class="push-left" src="./assets/img/logo195x30.png" alt="" style="padding-top: 8px">
-        </a>
-        <ul class="pull-right sf-menu" id="fh5co-primary-menu">
-            <li>
-                <a href="/tienda">Tienda Online</a>
-            </li>
-            <li>
-                <a href="/productos">Productos</a>
-            </li>
-            <li>
-                <a href="/creativa">Pasteleria Creativa</a>
-            </li>
-            <li>
-                <a href="/talleres">Talleres</a>
-            </li>
-            <li>
-                <a href="/nosotros">Nosotros</a>
-            </li>
-            <li>
-                <a href="/contacto">Contacto</a>
-            </li>
-        </ul>
-    </div>
+        <h1 id="fh5co-logo" class="pull-left">
+            <a href="index.php">
+                <img src="./assets/img/logo195x30.png" alt="">
+            </a>
+        </h1>
 
+        <nav id="fh5co-menu-wrap" role="navigation" class=" visible-sm visible-md visible-lg">
+            <ul class="sf-menu" id="fh5co-primary-menu">
+                <li>
+                    <a href="/tienda">Tienda Online</a>
+                </li>
+                <li>
+                    <a href="/productos">Productos</a>
+                    <ul class="fh5co-sub-menu">
+                        <li><a href="#">Bebidas calientes</a></li>
+                        <li><a href="#">Bebidas frias</a></li>
+                        <li><a href="#">Miel</a></li>
+                        <li><a href="#">Pasteleria fina</a></li>
+                        <li><a href="#">Te a granel</a></li>
+                        <li><a href="#">Tradicionales</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/creativa">Pasteleria Creativa</a>
+                    <ul class="fh5co-sub-menu">
+                        <li><a href="#">Bodas</a></li>
+                        <li><a href="#">Ocasiones</a></li>
+                        <li><a href="#">Para Ellas</a></li>
+                        <li><a href="#">Para Ellos</a></li>
+                        <li><a href="#">Para Niñas</a></li>
+                        <li><a href="#">Para Niños</a></li>
+                        <li><a href="#">Photo Cakes y Emojis</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/talleres">Talleres</a>
+                    <ul class="fh5co-sub-menu">
+                        <li><a href="#">Eventos</a></li>
+                        <li><a href="#">CCI</a></li>
+                        <li><a href="#">Recreo</a></li>
+                        <li><a href="#">Ventura Mall</a></li>
+                        <li><a href="#">Regalar una clase</a></li>
+                        <li><a href="#">Haz tu propio pastel</a></li>
+                        <li><a href="#">Regala un cupón</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/nosotros">Nosotros</a>
+                    <ul class="fh5co-sub-menu">
+                        <li><a href="#">Nosotros</a></li>
+                        <li><a href="#">Postúlate</a></li>
+                        <li><a href="#">Prensa</a></li>
+                        <li><a href="#">Testimonios</a></li>
+                        <li><a href="#">Nuestros Pasteles</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/contacto">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </div>
