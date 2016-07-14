0b23935e21a75cb6aa399ad273671b049dd4ec64
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-07-14 14:41:44 -0400

    Layout inicial de la seccion productos

--- views/productos/index.php
+++ views/productos/index.php
@@ -1,3 +1,53 @@
 <? require_once "./views/layout/menu.php" ?>
-<? require_once "./views/layout/construccion.php" ?>
+
+<div id="fh5co-main">
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-8 col-md-push-4" id="fh5co-content">
+                <div class="content-box animate-box">
+                    <h2>We Love Doing Free Stuffs</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam. Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam. Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.</p>
+
+                </div>
+                <div class="content-box animate-box">
+                    <h2>About This Template</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam. Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!</p>
+                    <p><img src="images/img_large_1.jpg" alt="Free HTML5 Template" class="img-responsive"></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam. Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.</p>
+
+                </div>
+            </div>
+
+            <div class="col-md-4 col-md-pull-8 left-sidebar" id="fh5co-sidebar">
+                <div class="sidebar-box animate-box">
+                    <h3 class="sidebar-heading"><span class="border"></span>Free Templates</h3>
+                    <ul class="sidebar-links">
+                        <li><a href="http://freehtml5.co/preview/?item=build-free-html5-bootstrap-template" target="_blank">Build</a></li>
+                        <li><a href="http://freehtml5.co/preview/?item=work-free-html5-template-bootstrap" target="_blank">Work</a></li>
+                        <li><a href="http://freehtml5.co/preview/?item=light-free-html5-template-bootstrap" target="_blank">Light</a></li>
+                        <li><a href="http://freehtml5.co/preview/?item=relic-free-html5-template-using-bootstrap" target="_blank">Relic</a></li>
+                        <li><a href="http://freehtml5.co/preview/?item=display-free-html5-template-using-bootstrap" target="_blank">Display</a></li>
+                        <li><a href="http://freehtml5.co/preview/?item=sprint-free-html5-template-bootstrap" target="_blank">Sprint</a></li>
+                    </ul>
+                </div>
+                <div class="sidebar-box animate-box">
+                    <h3 class="sidebar-heading"><span class="border"></span>Pargraph</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, aperiam placeat deserunt ullam magnam repudiandae reprehenderit animi aliquid odio ratione.</p>
+                    <p><a href="#" class="btn btn-outline btn-sm">Button</a></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+</div>
+
 <? require_once "./views/layout/footer.php" ?>
