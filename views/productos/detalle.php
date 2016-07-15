eb784e2214e82606e9d5b9f46ccf1a4689c1fa3a
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-07-15 11:53:58 -0400

    Anadiendo template para detalle de producto

---
+++ views/productos/detalle.php
@@ -0,0 +1,48 @@
+<? require_once "./views/layout/menu.php" ?>
+
+<div id="fh5co-main">
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-8 col-md-push-4" id="fh5co-content">
+                <div class="content-box animate-box">
+                    <h2>We Love Doing Free Stuffs</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam. Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam. Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.</p>
+
+                </div>
+                <div class="content-box animate-box">
+                    <h2>About This Template</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam. Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!</p>
+                    <p><img src="images/img_large_1.jpg" alt="Free HTML5 Template" class="img-responsive"></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam. Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.</p>
+
+                </div>
+            </div>
+
+            <div class="col-md-4 col-md-pull-8 left-sidebar" id="fh5co-sidebar">
+                <div class="sidebar-box animate-box">
+                    <h3 class="sidebar-heading"><span class="border"></span>Nuestros productos</h3>
+                    <ul class="sidebar-links">
+                        <?php $menuItems->printSubitems("productos");?>
+                    </ul>
+                </div>
+                <div class="sidebar-box animate-box">
+                    <h3 class="sidebar-heading"><span class="border"></span>Pargraph</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, aperiam placeat deserunt ullam magnam repudiandae reprehenderit animi aliquid odio ratione.</p>
+                    <p><a href="#" class="btn btn-outline btn-sm">Button</a></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+</div>
+
+<? require_once "./views/layout/footer.php" ?>
