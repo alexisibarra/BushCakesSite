c5a289bebe566fddd1571127221c2316c6f87000
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-07-18 13:28:33 -0400

    Ajustes para la presentacion de productos en resoluciones pequeñas

--- views/productos/detalle.php
+++ views/productos/detalle.php
@@ -3,34 +3,32 @@
 <div id="fh5co-main">
 
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-push-4" id="fh5co-content">
-                <div class="content-box animate-box">
-                    <h2><? echo $producto->name ?></h2>
-                    <p class="fh5co-category" style="font-size: 14px">
-                        <? echo $producto->description ?>
-                    </p>
+        <div class="col-md-8 col-md-push-4" id="fh5co-content">
+            <div class="content-box animate-box">
+                <h2><? echo $producto->name ?></h2>
+                <p class="fh5co-category" style="font-size: 14px">
+                    <? echo $producto->description ?>
+                </p>
 
-                    <div class="owl-carousel animate-box">
-                        <? foreach ($producto->images as $image) {?>
-                            <div class="item">
-                                <a href="<? echo $image?>" class="image-popup">
-                                    <img src="<? echo $image?>" alt="image">
-                                </a>
-                            </div>
-                        <? } ?>
-                    </div>
+                <div class="owl-carousel animate-box">
+                    <? foreach ($producto->images as $image) {?>
+                        <div class="item">
+                            <a href="<? echo $image?>" class="image-popup">
+                                <img src="<? echo $image?>" alt="image">
+                            </a>
+                        </div>
+                    <? } ?>
+                </div>
 
-                    <div class="fh5co-spacer fh5co-spacer-md"></div>
+                <div class="fh5co-spacer fh5co-spacer-md"></div>
 
-                    <div class="animate-box">
-                        <? echo $producto->info ?>
-                    </div>
+                <div class="animate-box">
+                    <? echo $producto->info ?>
                 </div>
             </div>
-
-            <? require './views/productos/sidebar.php'; ?>
         </div>
+
+        <? require './views/productos/sidebar.php'; ?>
     </div>
 
 </div>
--- views/productos/index.php
+++ views/productos/index.php
@@ -3,48 +3,43 @@
 <div id="fh5co-main">
 
     <div class="container">
-        <div class="row">
-
-            <div class="col-md-8 col-md-push-4" id="fh5co-content">
-                <div class="content-box animate-box">
-                    <h2>Nuestros productos</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic
-                        delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam.
-                        Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum
-                        nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa
-                        expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores
-                        ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.
-                    </p>
-
-                </div>
-
-<!--                <div class="fh5co-spacer fh5co-spacer-md"></div>-->
-
-                <div class="row fh5co-cards" id="fh5co-works" style="background-color: transparent">
-                    <?foreach ($productos as $item) {?>
-                        <div class="col-md-6 col-sm-6 col-xs-6 col-xxs-12 fh5co-work-item work-box">
-                            <div class="fh5co-card" href="#">
-                                <img src="<? echo $item->images[0] ?>" alt="Free HTML5 Bootstrap template" class="img-responsive">
-                                <div class="fh5co-card-body">
-                                    <h3><? echo $item->name ?></h3>
-                                    <p style="font-size: 0.8em"><? echo $item->description ?></p>
-                                    <a href="<? echo $item->link?>" class="link">Ve más</a>
-                                </div>
-                            </div>
-                        </div>
-                    <?}?>
-                </div>
+        <div class="col-md-8 col-md-push-4" id="fh5co-content">
+            <div class="content-box animate-box">
+                <h2>Nuestros productos</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dolor quas inventore hic
+                    delectus, temporibus vel voluptate nemo, repellat eaque nostrum ducimus numquam repudiandae nam.
+                    Quibusdam quaerat aspernatur commodi accusantium obcaecati pariatur vel eos quas vero quae rerum
+                    nemo nihil non laborum labore magni numquam adipisci voluptatum, voluptates soluta. Vel!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur optio iusto officiis culpa
+                    expedita rem nisi repudiandae quas, eveniet, neque nihil pariatur! Doloribus, sunt? Maiores
+                    ipsum temporibus consectetur voluptas, placeat perspiciatis officia, distinctio repellat earum.
+                </p>
 
             </div>
 
-            <? require './views/productos/sidebar.php'; ?>
+<!--                <div class="fh5co-spacer fh5co-spacer-md"></div>-->
 
+            <div class="row fh5co-cards" id="fh5co-works" style="background-color: transparent">
+                <?foreach ($productos as $item) {?>
+                    <div class="col-md-6 col-sm-6 col-xs-6 col-xxs-12 fh5co-work-item work-box">
+                        <div class="fh5co-card" href="#">
+                            <img src="<? echo $item->images[0] ?>" alt="Free HTML5 Bootstrap template" class="img-responsive">
+                            <div class="fh5co-card-body">
+                                <h3><? echo $item->name ?></h3>
+                                <p style="font-size: 0.8em"><? echo $item->description ?></p>
+                                <a href="<? echo $item->link?>" class="link">Ve más</a>
+                            </div>
+                        </div>
+                    </div>
+                <?}?>
+            </div>
 
         </div>
+
+        <? require './views/productos/sidebar.php'; ?>
     </div>
 
 
