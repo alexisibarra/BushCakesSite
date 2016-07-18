67d1f67d2207139e0980939d33b3b73c54196515
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-07-18 12:41:21 -0400

    Responsive home

--- views/home/hero.php
+++ views/home/hero.php
@@ -1,6 +1,6 @@
 <div class="container" style="margin-bottom: 50px">
     <div class="row">
-        <div class="col-sm-8" style="padding-right: 5px">
+        <div class="col-sm-8 hero-carousel">
             <div class="owl-carousel animate-box">
                 <? for ($i = 1; $i <= 10; $i++){?>
                     <div class="item">
@@ -11,17 +11,17 @@
                 <? } ?>
             </div>
         </div>
-        <div class="col-sm-4" style="padding-left: 0px; margin-bottom: 5px">
+        <div class="col-sm-4 col-xs-6 hero-news margin-bottom-5 ">
             <div class="animate-box">
                 <a href="http://order.carlosbakery.com">
-                    <img src="http://lorempixel.com/300/238" alt="Order Online, Pick Up In-store" title="Order Online, Pick Up In-store">
+                    <img src="http://lorempixel.com/300/238" class="img-responsive" alt="Order Online, Pick Up In-store" title="Order Online, Pick Up In-store">
                 </a>
             </div>
         </div>
-        <div class="col-sm-4" style="padding-left: 0px">
+        <div class="col-sm-4 col-xs-6 hero-news">
             <div class="animate-box">
                 <a href="http://order.carlosbakery.com">
-                    <img src="http://lorempixel.com/300/238" alt="Order Online, Pick Up In-store" title="Order Online, Pick Up In-store">
+                    <img src="http://lorempixel.com/300/238" class="img-responsive"  alt="Order Online, Pick Up In-store" title="Order Online, Pick Up In-store">
                 </a>
             </div>
         </div>
