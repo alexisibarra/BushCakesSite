5175dc73cfdcb31bd2801df8f20b7e7f8acfb49c
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-07-18 13:19:06 -0400

    Ajustando las proporciones de las noticias y agregando una mas

--- views/home/news.php
+++ views/home/news.php
@@ -1,21 +1,7 @@
 <div class="fh5co-cards animate-box news-section">
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 col-xs-12">
 
-            <? for ($i = 0; $i <= 1; $i++){?>
-                <div class="fh5co-card-small">
-                    <div class="image col-xs-4" style="padding:0px;">
-                        <img src="http://lorempixel.com/200/300/" alt="Free HTML5 Bootstrap template" class="img-responsive">
-                    </div>
-                    <div class="card-body col-xs-8">
-                        <h3 class="news-title">News</h3>
-                        <p class="news-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sunt porro delectus cum officia magnam.</p>
-                        <a href="" class="link">Read more</a>
-                    </div>
-                </div>
-            <?} ?>
-            </div>
             <div class="col-sm-6 col-xs-12">
                 <div class="fh5co-card" href="#">
                     <img src="http://lorempixel.com/445/320" alt="Free HTML5 Bootstrap template" class="img-responsive">
@@ -25,6 +11,20 @@
                         <a href="" class="link">Read more</a>
                     </div>
                 </div>
+            </div>
+            <div class="col-sm-6 col-xs-12">
+            <? for ($i = 0; $i <= 2; $i++){?>
+                <div class="fh5co-card-small">
+                    <div class="image col-xs-4" style="padding:0px;">
+                        <img src="http://lorempixel.com/220/220/" alt="Free HTML5 Bootstrap template" class="img-responsive">
+                    </div>
+                    <div class="card-body col-xs-8">
+                        <h3 class="news-title">News</h3>
+                        <p class="news-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sunt porro delectus cum officia magnam.</p>
+                        <a href="" class="link">Read more</a>
+                    </div>
+                </div>
+            <?} ?>
             </div>
         </div>
     </div>
